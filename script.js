0ea70ce46a9197546b1b1a8570f8d929d8d71a5d
João Pedro <joaopetrussousa@gmail.com>
2022-05-05 19:29:45 -0300

    Requisito 3(incompleto) e Requisito 4

---
+++ script.js
@@ -0,0 +1,19 @@
+/* login();
+function login(){ */
+    const botaoLogin = document.querySelector("#btn-login");    
+    const email = document.getElementById("email");
+    const senha = document.getElementById("password");
+
+    function validar(){
+        let campoEmail = /* "tryber@teste.com" */email;
+        let campoSenha = /* "123456" */senha;
+        console.log("teste");
+        if (campoEmail === "tryber@teste.com" && campoSenha === "123456") {
+            window.alert("Olá, Tryber!");
+        }else{
+            window.alert("Email ou senha inválidos.")
+        }
+    }
+
+    botaoLogin.addEventListener("click", validar())
+/* } */
